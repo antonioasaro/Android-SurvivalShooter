674b15b0c8cddc05d2ba16720fc2d8af1de2b9ed
antonioasaro <antonio_asaro@hotmail.com>
2018-01-29 18:25:42 -0500

    Hide Dpad & button in PC mode

---
+++ Assets/Scripts/Managers/ControlsOnOff.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlsOnOff : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+#if UNITY_ANDROID
+		CanvasGroup cg = GetComponent <CanvasGroup> ();
+		cg.interactable = true;
+		cg.alpha = 1;
+#endif
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
